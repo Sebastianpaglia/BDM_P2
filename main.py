026de476f7bead70990b204e8850a2cec7c4c1ec
gerard.sanchez.maltas <gerard.sanchez.maltas@estudiantat.upc.edu>
2023-06-07 19:11:44 +0200

    main

--- main.py
+++ main.py
@@ -1,20 +1,139 @@
-from LearnSqlToHdfsCollector import LearnSqlToHdfsCollector
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+import time
+from pymongo import MongoClient
 import os
+import glob
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
+"""spark = SparkSession \
+    .builder \
+    .master(f"local[*]") \
+    .appName("myApp") \
+    .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
+    .getOrCreate()
 
-# Add these lines to define the local folder paths
-idealista_path = os.path.join(base_dir, 'data', 'idealista')
+incomeRDD = spark.read.format("mongo") \
+    .option('uri', f"mongodb://localhost/test.test_bdm") \
+    .load() \
+    .rdd"""
 
-LOCAL_FOLDERS = [idealista_path]
+
+def load_collection(collection):
+    # Connect to MongoDB
+    client = MongoClient("mongodb://10.4.41.48")
+
+    # Access the desired database and collection
+    db = client["landing_zone"]
+    documents_collection = db[collection]
+    documents = list(documents_collection.find())
+    return documents
 
 
-HDFS_URL = 'http://10.4.41.48:9870/'
-hdfs_path = '/user/bdm/'
+def list_to_rdd(spark_connector, list_documents):
+    return spark_connector.sparkContext.parallelize(list_documents)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # Add these lines to copy local folders to HDFS
-    learn_sql_to_hdfs_collector = LearnSqlToHdfsCollector(hdfs_url=HDFS_URL)
-    local_folders = LOCAL_FOLDERS
-    learn_sql_to_hdfs_collector.copy_to_hdfs(local_folders)
+
+def add_columns(row, value: tuple):
+    return row + value
+
+
+spark = SparkSession.builder \
+    .master(f"local[*]") \
+    .appName("List to RDD") \
+    .getOrCreate()
+
+idealista_selected_columns = ['district', 'neighborhood', 'municipality', 'bathrooms',
+                              'distance', 'exterior', 'floor', 'latitude',
+                              'longitude', 'price', 'priceByArea', 'propertyType', 'rooms', 'size', 'status']
+
+data_idealista_path = 'data/idealista'
+date_folders = os.listdir(data_idealista_path)
+idealista_rdd = spark.sparkContext.parallelize([])
+for folder in date_folders[:10]:
+    search_pattern = os.path.join(data_idealista_path, '**/*.parquet')
+    parquet_file = glob.glob(search_pattern, recursive=True)
+    idealista_file_df = spark.read.parquet(*parquet_file).select(idealista_selected_columns)
+    columns = idealista_file_df.columns + ['year', 'month']
+    idealista_file_rdd = idealista_file_df.rdd
+    year_month = tuple([int(elem) for elem in folder.split('_')[:2]])
+    idealista_file_rdd = idealista_file_rdd.map(lambda x: add_columns(x, year_month))
+    idealista_rdd = idealista_rdd.union(idealista_file_rdd)
+
+data_incidents_path = 'data/incidents'
+incidents_rdd = spark.read.options(header='True', inferSchema='True', delimiter=',') \
+    .csv(data_incidents_path).rdd
+
+# Convert DataFrame to RDD
+income_rdd = list_to_rdd(spark, load_collection('income'))
+income_neigh = list_to_rdd(spark, load_collection('income_lookup_neighborhood'))
+rent_neigh = list_to_rdd(spark, load_collection('rent_lookup_neighborhood'))
+
+"""income_rdd_by_key = income_rdd.flatMap(lambda x:
+                   [{"neigh_name": x["neigh_name "],
+                     "district_name": x["district_name"],
+                     "year": d["year"],
+                     "pop": d["pop"],
+                     "RFD": d["RFD"]}
+                    for d in x["info"]]).keyBy(lambda x: x['neigh_name'])
+
+income_neigh_by_key = income_neigh.keyBy(lambda x: x['neighborhood'])
+
+income_joined = income_rdd_by_key.join(income_neigh_by_key)
+income_conciled = income_joined.map(lambda x: (
+                                x[1][1]['neighborhood_reconciled'],
+                                x[1][1]["_id"], x[1][0]['district_name'],
+                                x[1][0]['year'],
+                                x[1][0]['pop'],
+                                x[1][0]['RFD']
+                            )
+                  )
+
+columns_income = ['neighborhood',
+                  'id_neighborhood',
+                  'district',
+                  'year',
+                  'population',
+                  'RFD']
+
+df_income = income_conciled.toDF(columns_income)
+df_income.show()"""
+# df_income.write.parquet('income_formatted_zone')
+
+# Joining idealista with lookup
+rent_neigh_bykey = rent_neigh.keyBy(lambda x: x["ne"])
+idealista_rdd_bykey = idealista_rdd.keyBy(lambda x: x[1])
+idealista_rdd_joined = idealista_rdd_bykey.join(rent_neigh_bykey)
+idealista_conciled = idealista_rdd_joined.map(
+    lambda x: (x[1][0][0],) + (x[1][1]["_id"], x[1][1]["ne_re"]) + x[1][0][2:])
+
+incidents_rdd_by_key = incidents_rdd \
+    .groupBy(lambda x: x[2:9]) \
+    .mapValues(lambda x: sum(int(v[9]) for v in x if v[9] is not None)) \
+    .map(lambda x: x[0] + (x[1],)) \
+    .keyBy(lambda x: x[3])
+
+income_neigh_by_key = income_neigh.keyBy(lambda x: x['neighborhood'])
+
+incidents_rdd_joined = incidents_rdd_by_key.join(income_neigh_by_key)
+incidents_conciled = incidents_rdd_joined.map(
+    lambda x: (x[1][1]["neighborhood_reconciled"], x[1][1]["_id"]) + tuple(x[1][0])[:3] + tuple(x[1][0])[4:])
+
+idealista_conciled_bykey = idealista_conciled.map(lambda x: (x[1] + "_" + str(x[16]) + "_" + str(x[17]), x))
+incidents_conciled_bykey = incidents_conciled.map(lambda x: (x[1] + "_" + x[5] + "_" + x[6], x))
+rdd1 = idealista_conciled_bykey.join(incidents_conciled_bykey)
+rdd1_final = rdd1.map(lambda x: x[1][0] + (x[1][1][8], ))
+columns_idealista_incidents = ['district',
+                               'id_neighborhood',
+                               'neighborhood',
+                               'city',
+                               'bathrooms',
+                               'distance',
+                               'exterior',
+                               'floor', 'latitude',
+                               'longitude', 'price', 'priceByArea', 'propertyType', 'rooms',
+                               'size', 'status', 'year', 'month', 'incidents']
+
+df_idealista_incidents = rdd1_final.toDF(columns_idealista_incidents)
+df_idealista_incidents.write.parquet('idealista_incidents_formatted_zone')
+# Stop the SparkSession
+spark.stop()

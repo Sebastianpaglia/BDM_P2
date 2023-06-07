af9ce62bb063cb15aeffecdafbcca3a0c6d3493b
Sebastian Paglia <74685763+Sebastianpaglia@users.noreply.github.com>
2023-06-07 11:18:35 +0200

    create .py initial files

---
+++ LearnSqlToHdfsCollector.py
@@ -0,0 +1,30 @@
+from hdfs import InsecureClient
+import os
+import datetime
+
+class LearnSqlToHdfsCollector:
+    def __init__(self, hdfs_url: str):
+        self.hdfs_directory = 'idealista'
+        self.hdfs_client = InsecureClient(hdfs_url, user='bdm')
+
+    def copy_to_hdfs(self, local_folders: list):
+        if self.hdfs_client.content(self.hdfs_directory, strict=False) is None:
+            self.hdfs_client.makedirs(self.hdfs_directory, permission=777)
+
+        for local_folder in local_folders:
+            folder_name = os.path.basename(local_folder)
+            hdfs_folder_path = self.hdfs_directory + '/' + folder_name
+
+            # Create HDFS directory
+            self.hdfs_client.makedirs(hdfs_folder_path, permission=777)
+
+            # Copy local files to HDFS
+            for root, dirs, files in os.walk(local_folder):
+                current_extraction = datetime.datetime.now().strftime(
+                    '%Y_%m_%d_%H_%M_%S')
+                for file in files:
+                    local_path = os.path.join(root, file)
+                    file_name, ext = os.path.splitext(file)
+                    hdfs_path = hdfs_folder_path + '/' + current_extraction + '/' + file_name + '_' + current_extraction + ext
+                    with open(local_path, 'rb') as f:
+                        self.hdfs_client.write(hdfs_path, f, overwrite=True)
---
+++ main.py
@@ -0,0 +1,20 @@
+from LearnSqlToHdfsCollector import LearnSqlToHdfsCollector
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Add these lines to define the local folder paths
+idealista_path = os.path.join(base_dir, 'idealista')
+
+LOCAL_FOLDERS = [idealista_path]
+
+
+HDFS_URL = 'http://10.4.41.48:9870/'
+hdfs_path = '/user/bdm/'
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Add these lines to copy local folders to HDFS
+    learn_sql_to_hdfs_collector = LearnSqlToHdfsCollector(hdfs_url=HDFS_URL)
+    local_folders = LOCAL_FOLDERS
+    learn_sql_to_hdfs_collector.copy_to_hdfs(local_folders)
